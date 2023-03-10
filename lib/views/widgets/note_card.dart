6e15478602f58dce9f646bbf8d621f38e2aef74b
sohaila <habibsohaila970@gmail.com>
2023-03-10 23:18:36 +0200

    note_creation

--- lib/views/widgets/note_card.dart
+++ lib/views/widgets/note_card.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:note_app1/models/note_model.dart';
 
 class NoteCard extends StatelessWidget {
-  const NoteCard({super.key});
-
+  const NoteCard({super.key,required this.note});
+final NoteModel note; 
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -20,22 +20,22 @@ class NoteCard extends StatelessWidget {
         ListTile(
           title:Padding(
             padding: EdgeInsets.only(top:16),
-            child: Text("Flutter",
+            child: Text(note.title,
           style: GoogleFonts.ubuntu(textStyle: TextStyle(color: Colors.black),fontSize: 28),)),
          
           subtitle:Padding(
           padding:EdgeInsets.only(top: 11),
-          child: Text("Finish Note APP",
+          child: Text(note.subTitle,
           style: GoogleFonts.ubuntu(
         textStyle: TextStyle(color: Colors.black.withOpacity(0.5),fontSize: 20),),)),
           trailing: IconButton(//padding:const EdgeInsets.only(top:10),
             color: Colors.black,
             icon:const Icon( Icons.delete),iconSize: 30, 
-            onPressed: () {  },),
+            onPressed: () {note.delete();  },),
         )
     ,Padding(
      padding: const EdgeInsets.only(right: 16),
-      child: Text("Month....Day",
+      child: Text(note.date,
      style: GoogleFonts.ubuntu(
      textStyle: TextStyle(color: Colors.black.withOpacity(0.5)),))) ]),
     );
